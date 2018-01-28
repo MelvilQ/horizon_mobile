e7fe4e28b58c0b629c971ba43581857a6d244c09
melvil <paradise-interactive@web.de>
2018-01-28 11:51:44 +0100

    dictionary lookup of unknown words using iframe overlay

--- app/src/main/assets/reader.js
+++ app/src/main/assets/reader.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
 	$('#text').children('span').click(function(){
 		var meaning = $(this).data('meaning');
-		if(meaning)
+		if(meaning){
 			Android.showMeaning(meaning);
+		} else {
+			var word = $(this).text();
+			openDictionary(word);
+		}
 	});
 });
+
+function openDictionary(word){
+	closeDictionary();
+
+	var url = $('#dict').data('url');
+	if(!url || !word)
+		return;
+	url = url.replace('$$$', word);
+
+	var iframe = $('<iframe>').attr({src: url, frameborder: 0, allowfullscreen: ''});
+	iframe.on('load', function(){
+        this.style.visibility = 'visible';
+    });
+
+	var close = $('<div>').attr({id: 'close', href: '#'}).text('CLOSE');
+	close.click(function(e){
+		e.preventDefault();
+		closeDictionary();
+	});
+
+	$('#dict').append(iframe);
+	$('#dict').append(close);
+	$('#dict').show();
+}
+
+function closeDictionary(){
+	$('#dict').hide();
+	$('#dict').empty();
+}
